a54ebbbe567120aa32f8a4022cf9ffe00a9a10d2
fac a <fac@gmail.com>
2024-08-30 13:34:19 +0800

    feat: fix SenderDomain

--- background.js
+++ background.js
@@ -5,16 +5,20 @@ var SearchForSenderEmailAddress = {
 		if (info.selectedMessages && info.selectedMessages.messages.length > 0) {
 			this.msgs=info.selectedMessages.messages;
 			this.msgs.forEach( async mh => {
+				console.log('address 333');
 				var author = mh.author.replaceAll('\"', '');
+				console.log('address 333  - author:' + author);
 				var array_author = author.split('<');
 				var author_emailaddress;
 				if (array_author[1] && array_author[1].length > 0)
 				{
+					console.log('address 444');
 					author_emailaddress = array_author[1].trim();
 					author_emailaddress = author_emailaddress.substring(0,(author_emailaddress.length) - 1);
 				}
 				else
 				{
+					console.log('address 444-2');
 					author_emailaddress = mh.author;
 				}
 				this.search(author_emailaddress, true, false);
@@ -74,20 +78,46 @@ var SearchForSenderName = {
 
 var SearchForSenderDomain = {
 	getAddress: async function(info) {
+		console.log('domain 111');
 		if (info.selectedMessages && info.selectedMessages.messages.length > 0) {
+			console.log('domain 222');
 			this.msgs=info.selectedMessages.messages;
 			this.msgs.forEach( async mh => {
+				console.log('domain 333');
 				var author = mh.author.replaceAll('\"', '');
-				var array_author = author.split('<');
+				console.log('domain 333 - author:' + author);
+				// format:
+				// 1. marco.h@aromate.com
+				// 2. pe john <ask79jb@jetbean.com.tw>
+				
+				var author_domain = author.split('@')[1].split('>')[0];
+				if (author_domain)
+				{
+					this.search(author_domain, true, false);
+				}
+				else {
+					console.log('SearchFor: SenderDomain ERROR!');
+				}
+				
+				/*var array_author = author.split('<');
+				console.log('domain 333 - array_author:' + array_author);
 				var author_domain;
 				if (array_author[1] && array_author[1].length > 0)
 				{
+					console.log('domain 444');
 					author_domain = array_author[1].trim();
 					author_domain = author_domain.substring(0,(author_domain.length) - 1);				
+				}
+				else //single author
+				{
+					console.log('domain 444-2');
+					author_domain = mh.author;
+
 					var pos = author_domain.search("@");
 					author_domain = author_domain.substring(pos, author_domain.length);
 					this.search(author_domain, true, false);
 				}
+				//this.search(author_emailaddress, true, false);*/
 			});
 		} 
 		else {
@@ -108,7 +138,9 @@ var SearchForSenderDomain = {
 
 var SearchForSubject = {
 	getAddress: async function(info) {
+		//console.log('222');
 		if (info.selectedMessages && info.selectedMessages.messages.length > 0) {
+			//console.log('222-1');
 			this.msgs=info.selectedMessages.messages;
 			this.msgs.forEach( async mh => {
 				this.search(mh.subject, false, true);
@@ -120,7 +152,8 @@ var SearchForSubject = {
 		return;
 	},
     search : function(searchTerm, useSenderAsCriteria, useSubjectAsCriteria) {
-		 browser.mailTabs.setQuickFilter({
+		
+		browser.mailTabs.setQuickFilter({
 			text: {
 				text: searchTerm,
 				author : useSenderAsCriteria,
